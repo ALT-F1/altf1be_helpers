83fd68654693c4ce325b6048825d6f700721d37c
Abdelkrim Boujraf <abo+git@alt-f1.be>
2020-06-25 00:44:32 +0200

    build: add the AltF1BeJSONHelpers in the package

--- altf1be_helpers/__init__.py
+++ altf1be_helpers/__init__.py
@@ -3,6 +3,9 @@
 # Copyright (c) ALT-F1 SPRL, Abdelkrim Boujraf. All rights reserved.
 # Licensed under the EUPL License, Version 1.2. See LICENSE in the project root for license information.
 
-from .altf1be_helpers import AltF1BeHelpers
+from .altf1be_helpers import AltF1BeHelpers, AltF1BeJSONHelpers
 
-__all__ = ["AltF1BeHelpers"]
+__all__ = [
+    "AltF1BeHelpers",
+    "AltF1BeJSONHelpers"
+]
